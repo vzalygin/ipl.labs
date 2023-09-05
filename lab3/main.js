78b7e99ab9d3c5268b15008f297ea0376424da2e
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2023-09-05 15:21:55 +0300

    lab3

---
+++ lab3/main.js
@@ -0,0 +1,17 @@
+const treeBtn = document.getElementById("tree-btn");
+const treeContainer = document.getElementById("tree-container");
+
+const drawTree = (node, tabs) => {
+  if (node.tagName !== undefined) {
+    const text = ".&nbsp".repeat(tabs) + node.tagName;
+    console.log(text);
+    const p = document.createElement("p");
+    p.innerHTML = text;
+    treeContainer.appendChild(p);
+    node.childNodes.forEach((c) => drawTree(c, tabs + 1));
+  }
+};
+
+treeBtn.addEventListener("click", (event) => {
+  drawTree(document.body, 0);
+});
