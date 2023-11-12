961bd2881f97f108ef95009eeae1152b8c971ae2
Slava Zalygin <vya.zalygin@gmail.com>
2023-11-12 19:35:10 +0000

    lab8

--- lab8/app/controllers/compute_controller.rb
+++ lab8/app/controllers/compute_controller.rb
@@ -2,6 +2,14 @@ class ComputeController < ApplicationController
   def input
   end
 
+  def is_palindrome(x)
+    x = x.to_s
+    return x == x.reverse
+  end
+
   def view
+    n = params[:n].to_i
+    @result = (0..n)
+      .select { |i| is_palindrome(i*i) }
   end
 end
