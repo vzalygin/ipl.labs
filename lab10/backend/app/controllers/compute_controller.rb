a7da2935d755a12e8a6a29e962e41bdca7e7309a
Slava Zalygin <vya.zalygin@gmail.com>
2023-11-22 12:49:54 +0000

    rk2

--- lab10/backend/app/controllers/compute_controller.rb
+++ lab10/backend/app/controllers/compute_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'libxml'
+
 class ComputeController < ApplicationController
   def input; end
 
@@ -12,5 +14,16 @@ class ComputeController < ApplicationController
     n = params[:n].to_i
     @result = (0..n)
               .select { |i| is_palindrome(i * i) }
+
+    respond_to do |format|
+      format.html
+      format.json do
+      render json:
+      {type: @result.class.to_s, value: @result}
+      end
+      format.xml do 
+        { @result }.to_xml
+      end
+    end
   end
 end
--- rk2/app/controllers/main_controller.rb
+++ rk2/app/controllers/main_controller.rb
@@ -1,12 +1,31 @@
+# frozen_string_literal: true
+
 class MainController < ApplicationController
   def index
-    redirect_to "/main/input"
+    redirect_to '/main/input'
   end
-  
-  def input
+
+  def input; end
+
+  def replace(after)
+    max_v = after.max
+    min_v = after.min
+    i = if !after.find_index(&:negative?).nil?
+          after.find_index(&:negative?)
+        else
+          after.find_index { |x| x == min_v }
+        end
+    after[i] = max_v
+    after
   end
 
   def result
-    
+    arr_t = params[:arr_t].split.map(&:to_i)
+    @before = arr_t
+    @after = if !arr_t.empty?
+               replace(arr_t.map(&:clone))
+             else
+               []
+             end
   end
 end
